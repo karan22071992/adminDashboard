cd75a5300c8fca6b0f526dcbdaab5e6cdb5f4fcd
karan22071992 <karan22071992@gmail.com>
2022-07-20 15:19:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
-import "./app.css";
+import "./App.css";
 import Home from "./pages/home/Home";
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
@@ -13,38 +13,36 @@ import NewProduct from "./pages/newProduct/NewProduct";
 function App() {
   return (
     <Router>
-    <div >
       <Topbar />
-      <div className="container" >
+      <div className="container">
         <Sidebar />
-        <Switch >
-          <Route exact path="/"  >
+        <Switch>
+          <Route exact path="/">
             <Home />
           </Route>
-          <Route path="/users" >
+          <Route path="/users">
             <UserList />
           </Route>
-          <Route path="/user/:userId" >
+          <Route path="/user/:userId">
             <User />
           </Route>
-          <Route path="/newUser" >
+          <Route path="/newUser">
             <NewUser />
           </Route>
-          <Route path="/products" >
+          <Route path="/products">
             <ProductList />
           </Route>
-          <Route path="/product/:productId" >
+          <Route path="/product/:productId">
             <Product />
           </Route>
-          <Route path="/newproduct" >
+          <Route path="/newproduct">
             <NewProduct />
           </Route>
         </Switch>
-        
       </div>
-    </div>
     </Router>
   );
 }
 
 export default App;
+
